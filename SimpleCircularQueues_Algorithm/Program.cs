e29046f6dfdc002dcfbb9c613a2f6d752b76fe24
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-12-16 17:42:49 +0700

    condition to raverse the array till the last element present in the queue

--- SimpleCircularQueues_Algorithm/Program.cs
+++ SimpleCircularQueues_Algorithm/Program.cs
@@ -101,7 +101,15 @@ namespace SimpleCircularA
                     Console.Write(queue_array[FRONT_position] + "   ");
                     FRONT_position++;
                 }
-               
+                // Set the FRONT position to the first element of the array.
+                FRONT_position = 0;
+                /*Traverse the array till the last element present in the queue. */
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "   ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
             }
         }
     }
