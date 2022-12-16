e1b17f2027aa7a4541737ffe7ca76e254c860d11
Muhammad Ayash <muhammad.ayash.ft21@mail.umy.ac.id>
2022-12-16 17:23:44 +0700

    Create class Queues, then create variable

--- SimpleCircularQueues_Algorithm/Program.cs
+++ SimpleCircularQueues_Algorithm/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCircularQueues_Algorithm
+{
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        
+    }
+    
+}
